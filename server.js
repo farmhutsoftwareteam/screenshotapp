de6de0dbb99f6e371f9c1fd2decf37591c31e304
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-07-25 01:14:37 -0400

    change port

--- server.js
+++ server.js
@@ -35,8 +35,8 @@ app.get('/pdf', async (req, res) => {
   }
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`);
+  console.log(`App listening on port  ${port}`);
 });
 //will write thus code will it be compiled by this system>
