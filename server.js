84c368c109b87c22047d8cc1da34ca4a7342bd1b
farmhutsoftwareteam <108745208+farmhutsoftwareteam@users.noreply.github.com>
2023-07-25 03:01:58 -0400

    add headless

--- server.js
+++ server.js
@@ -1,16 +1,30 @@
 const express = require('express');
 const app = express();
+const IS_PRODUCTION = process.env.NODE_ENV === 'production';
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+
+const getBrowser = () => 
+IS_PRODUCTION
+?
+puppeteer.connect({ browserWSEndpoint: 'wss://chrome.browserless.io?token=4e8385cf-4e0e-47ed-84a3-8b41def51202' })
+:
+puppeteer.launch();
+
+
 
 
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
 app.get('/pdf', async (req, res) => {
+
+
+
   const url = req.query.url;
+  let browser = null;
   try {
-    const browser = await puppeteer.launch();
+    const browser = await getBrowser();
     const page = await browser.newPage();
 
     await page.goto(url, { timeout: 6000000, waitUntil: 'networkidle2' });
